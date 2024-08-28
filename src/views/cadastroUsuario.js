b79c76889306f197e83dc50e9e1393144b5318a6
Bruna Zorzanello <brunazorzanello@gmail.com>
2024-08-28 15:52:26 -0300

    adicionando as rotas no navbar

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -16,47 +16,46 @@ class CadastroUsuario extends React.Component  {
     }
 
     render(){
-        return(
-            <div className="container">           
-                <Card title="Cadastro de Usuário">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="bs-component">
-                                <FormGroup label="Nome: *" htmlFor="inputNome">
-                                    <input type="text"
-                                            id="inputNome"
-                                            className="form-control"
-                                            name="nome"
-                                            onChange={e => this.setState({nome: e.target.value})}/>
-                                </FormGroup>
-                                <FormGroup label="Email: *" htmlFor="inputEmail">
-                                    <input type="email"
-                                            id="Email"
-                                            className="form-control"
-                                            name="email"
-                                            onChange={e => this.setState({email: e.target.value})}/>
-                                </FormGroup>
-                                <FormGroup label="Senha: *" htmlFor="inputSenha">
-                                    <input type="password"
-                                            id="inputSenha"
-                                            className="form-control"
-                                            name="senha"
-                                            onChange={e => this.setState({senha: e.target.value})}/>
-                                </FormGroup>
-                                <FormGroup label="Repita a Senha: *" htmlFor="inputSenhaRepeticao">
-                                    <input type="password"
-                                            id="inputSenhaRepeticao"
-                                            className="form-control"
-                                            name="senha"
-                                            onChange={e => this.setState({senhaRepeticao: e.target.value})}/>
-                                </FormGroup>
-                                <button onClick="this.cadastrar" type="button" className="btn btn-success button-spacing">Salvar</button>
-                                <button type="button" className="btn btn-danger button-spacing">Cancelar</button>
-                            </div>
+        return(          
+            <Card title="Cadastro de Usuário">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="bs-component">
+                            <FormGroup label="Nome: *" htmlFor="inputNome">
+                                <input type="text"
+                                        id="inputNome"
+                                        className="form-control"
+                                        name="nome"
+                                        onChange={e => this.setState({nome: e.target.value})}/>
+                            </FormGroup>
+                            <FormGroup label="Email: *" htmlFor="inputEmail">
+                                <input type="email"
+                                        id="Email"
+                                        className="form-control"
+                                        name="email"
+                                        onChange={e => this.setState({email: e.target.value})}/>
+                            </FormGroup>
+                            <FormGroup label="Senha: *" htmlFor="inputSenha">
+                                <input type="password"
+                                        id="inputSenha"
+                                        className="form-control"
+                                        name="senha"
+                                        onChange={e => this.setState({senha: e.target.value})}/>
+                            </FormGroup>
+                            <FormGroup label="Repita a Senha: *" htmlFor="inputSenhaRepeticao">
+                                <input type="password"
+                                        id="inputSenhaRepeticao"
+                                        className="form-control"
+                                        name="senha"
+                                        onChange={e => this.setState({senhaRepeticao: e.target.value})}/>
+                            </FormGroup>
+                            <button onClick="this.cadastrar" type="button" className="btn btn-success button-spacing">Salvar</button>
+                            <button type="button" className="btn btn-danger button-spacing">Cancelar</button>
                         </div>
                     </div>
-                </Card>
-            </div>
+                </div>
+            </Card>
+           
         )
     }
 }
