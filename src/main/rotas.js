37d3022ed4ffa692fb33ae1efae753d106655876
Bruna Zorzanello <brunazorzanello@gmail.com>
2024-09-20 10:49:55 -0300

    implementação para isolar o login

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, HashRouter} from 'react-router-dom';
+import { Route, Switch, HashRouter, Redirect} from 'react-router-dom';
 import CadastroUsuario from "../views/cadastro-usuario";
 import Login from '../views/login';
 import Home from '../views/home';
@@ -7,14 +7,34 @@ import consultaLancamentos from "../views/lancamentos/consulta-lancamentos";
 import cadastroLancamentos from "../views/lancamentos/cadastro-lancamentos";
 
 function Rotas() {
+
+    const isUsuarioAutenticado = () => {
+        return true;
+    }
+
+    function RotaAutenticada( { component: Component, ...props }){
+        return (
+            <Route {...props} render={ (componentsPropos) => {
+                if(isUsuarioAutenticado()){
+                    return(
+                        <Component { ...componentsPropos} />
+                    )
+                } else {
+                    return (
+                        <Redirect to={{pathname: 'login', state: { from: componentsPropos.location}}}/>
+                    )
+                }
+            }}/>
+        )
+    }
     return(
         <HashRouter>
             <Switch>
-                <Route path="/home" component={Home}/>
                 <Route path="/login" component={Login}/>
                 <Route path="/cadastro-usuario" component={CadastroUsuario}/>
-                <Route path="/consulta-lancamento" component={consultaLancamentos}/>
-                <Route path="/cadastro-lancamento/:id?" component={cadastroLancamentos}/>
+                <RotaAutenticada path="/home" component={Home}/>                
+                <RotaAutenticada path="/consulta-lancamento" component={consultaLancamentos}/>
+                <RotaAutenticada path="/cadastro-lancamento/:id?" component={cadastroLancamentos}/>
             </Switch>
         </HashRouter>
     )
